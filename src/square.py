c257e856d61e00325b569ef38bbbf45ea4546a81
Smartlee <99546412+smartlee247@users.noreply.github.com>
2022-02-13 15:22:14 +0200

    Expanding the python code

--- src/square.py
+++ src/square.py
@@ -3,3 +3,11 @@ def my_squar(x):
 
 print(my_squar(3))
 
+int age =int(input("What is your age: "))
+if age ==33:
+	print("you can still learn programming")
+elif age < 33:
+	print("You are young enough enough to learn anything")
+elif age <33:
+	print("You can learn anything as much as you have the zeel, we can't guarantee")
+
