b3322dc394cf1c98da16bdc0ba61e6d196344de9
Smart Agwu <Smart Lee>
2022-03-12 09:56:00 +0200

    more updates

--- pyth.py
+++ pyth.py
@@ -2,7 +2,3 @@ def myth(x):
 	"""This script takes a value and return the square of it"""
 	return(x**3)
 print(myth(2))
-print("you are awesome")
-
-
->>>>>>> c3b3ef47db167799d4cf1bb04dfe0e6bbac93fd5
